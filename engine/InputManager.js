9b31cea5fa253660baab268f1865275aa7f0a7ca
NotTimTam <timothyrp6@gmail.com>
2024-07-03 16:55:10 -0700

    Implemented pointer lock API.

--- engine/InputManager.js
+++ engine/InputManager.js
@@ -14,6 +14,44 @@ class InputManager {
 		this.__eventListeners = [];
 	}
 
+	/**
+	 * Get the pointer lock status.
+	 */
+	get hasPointerLock() {
+		const { element } = this.runtime.renderer;
+		if (document.pointerLockElement === element) return true;
+		else return false;
+	}
+
+	/**
+	 * Initiate a pointer lock request. Pointer lock cannot be achieved unless the user clicks the screen after this method is called.
+	 */
+	async requestPointerLock() {
+		const { element } = this.runtime.renderer;
+
+		const initiatePointerLock = () => {
+			if (!this.hasPointerLock) element.requestPointerLock();
+		};
+
+		const lockChangeAlert = () => {
+			if (document.pointerLockElement === element) {
+				this.mouse = { buttons: {} };
+			}
+		};
+
+		document.body.addEventListener("click", initiatePointerLock);
+
+		document.addEventListener(
+			"pointerlockerror",
+			(e) => {
+				console.error("Pointer lock request failed.", e);
+			},
+			false
+		);
+
+		document.addEventListener("pointerlockchange", lockChangeAlert, false);
+	}
+
 	__formatKey(key) {
 		if (key === " ") return "space";
 		else if (key === "ArrowUp") return "up";
@@ -24,12 +62,21 @@ class InputManager {
 		return key.toLowerCase();
 	}
 
+	/**
+	 * Calls when a key is pushed.
+	 * @param {Event} event The listener's event.
+	 */
 	__onKeyDown(event) {
 		const { key, keyCode } = event;
 
 		this.keyboard.keys[this.__formatKey(key)] = true;
 		this.keyboard.keyCodes[keyCode] = true;
 	}
+
+	/**
+	 * Calls when a key is released.
+	 * @param {Event} event The listener's event.
+	 */
 	__onKeyUp(event) {
 		const { key, keyCode } = event;
 
@@ -37,6 +84,10 @@ class InputManager {
 		this.keyboard.keyCodes[keyCode] = false;
 	}
 
+	/**
+	 * Calls when a mouse button is pushed.
+	 * @param {Event} event The listener's event.
+	 */
 	__onMouseDown(event) {
 		const { button } = event;
 
@@ -52,6 +103,11 @@ class InputManager {
 				break;
 		}
 	}
+
+	/**
+	 * Calls when a mouse button is released.
+	 * @param {Event} event The listener's event.
+	 */
 	__onMouseUp(event) {
 		const { button } = event;
 
@@ -67,6 +123,11 @@ class InputManager {
 				break;
 		}
 	}
+
+	/**
+	 * Calls when the mouse is moved on screen.
+	 * @param {Event} event The listener's event.
+	 */
 	__onMouseMove(event) {
 		const { clientX, clientY, movementX, movementY } = event;
 
@@ -90,23 +151,31 @@ class InputManager {
 		const [rX, rY] = [clientX - canvasX, clientY - canvasY];
 		const [relX, relY] = [rX / canvasWidth, rY / canvasHeight];
 
-		this.mouse.velocity = [movementX, movementY];
-		this.mouse.rawX = clientX;
-		this.mouse.rawY = clientY;
-		this.mouse.canvasX = rX;
-		this.mouse.canvasY = rY;
-		this.mouse.x = clamp(
-			Math.round(relX * characterWidth),
-			0,
-			characterWidth
-		);
-		this.mouse.y = clamp(
-			Math.round(relY * characterHeight),
-			0,
-			characterHeight
-		);
+		if (this.hasPointerLock) {
+			this.mouse.velocity = [movementX, movementY];
+		} else {
+			this.mouse.velocity = [movementX, movementY];
+			this.mouse.rawX = clientX;
+			this.mouse.rawY = clientY;
+			this.mouse.canvasX = rX;
+			this.mouse.canvasY = rY;
+			this.mouse.x = clamp(
+				Math.round(relX * characterWidth),
+				0,
+				characterWidth
+			);
+			this.mouse.y = clamp(
+				Math.round(relY * characterHeight),
+				0,
+				characterHeight
+			);
+		}
 	}
 
+	/**
+	 * Manages different events firing, and maps them to the proper method.
+	 * @param {Event} event The listener's event.
+	 */
 	__onEvent(event) {
 		if (event instanceof MouseEvent) {
 			const { type } = event;
